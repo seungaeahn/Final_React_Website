b490e20311da855341162bf1971c6b2cd475798c
seungaeahn <143372285+seungaeahn@users.noreply.github.com>
2023-12-28 11:14:09 +0900

    12/28

--- client/src/Todo/Todo2.js
+++ client/src/Todo/Todo2.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import CreatePags from "./CreatePage";
+import ListPage from "./ListPage";
 function Todo() {
   //js로 state 상태관리
 
   const Home = () => <div>Home</div>;
-  const [action, setAction] = useState([]);
+  const [actions, setActions] = useState([]);
+
+  //생성하기
+  const addAction = (newAction) => {
+    setActions([...actions, newAction]);
+  };
+
+  //삭제하기
+  const deleteAction = (id) => {
+    setActions(actions.filter((actions) => actions.id !== id));
+  };
   return (
     <Router>
       <div>
@@ -20,10 +31,12 @@ function Todo() {
           </ul>
         </nav>
         <Routes>
-          <Route path="/" element={Todo}>
-            메인
-          </Route>
-          <Route path="/create" element={CreatePags}>
+          <Route
+            path="/"
+            element={<ListPage actions={actions} deleteAction={deleteAction} />}
+          />
+
+          <Route path="/create" element={<CreatePags action={addAction} />}>
             생성
           </Route>
         </Routes>
